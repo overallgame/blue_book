fcc54fcbe7698a7ac129e415c36a8722a4c1d13c
overfloatGame <3364295743@qq.com>
2026-06-08 13:03:26 +0800

    创建TokenCache和AuthDataStoreImpl，DataStore替代SharedPreferences存储登录态

--- data-local/src/main/java/com/example/blue_book/di/LocalModule.kt
+++ data-local/src/main/java/com/example/blue_book/di/LocalModule.kt
@@ -2,6 +2,8 @@ package com.example.blue_book.di
 
 import com.example.blue_book.data.local.db.resource.user.UserLocalDataResource
 import com.example.blue_book.data.local.db.resource.user.UserLocalDataResourceImpl
+import com.example.blue_book.data.local.preference.AuthDataStoreImpl
+import com.example.blue_book.data.local.preference.AuthPreferences
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,5 +17,9 @@ abstract class LocalModule {
     @Binds
     @Singleton
     abstract fun bindUserLocalDataResource(impl: UserLocalDataResourceImpl): UserLocalDataResource
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthPreferences(impl: AuthDataStoreImpl): AuthPreferences
 }
 
